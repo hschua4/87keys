7938e7cb66c34078765df0139bd0b34dea8a0407
Hai Siang <haisiang.chua@gmail.com>
2021-06-13 12:50:56 +0800

    Add colors to the console

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,14 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI, {
-			userUnifiedTopology: true,
+			useUnifiedTopology: true,
 			useNewUrlParser: true,
 			useCreateIndex: true,
 		});
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
+		console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
 	} catch (err) {
-		console.error(`Error: ${error.message}`);
+		console.error(`Error: ${err.message}`.red.underline.bold);
 		process.exit(1);
 	}
 };
